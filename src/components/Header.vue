a08d0858b84e40f704f1272cdff60e624e4d6cfe
Alexander Skiba <skibaalex1@gmail.com>
2020-09-07 21:22:00 -0500

    imported all the goods

---
+++ src/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+    <header id="header" class="header">
+    <div class="header-top bg-theme-colored sm-text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="widget no-border m-0">
+              <ul class="styled-icons icon-dark icon-circled icon-theme-colored icon-sm flip sm-pull-none sm-text-center mt-sm-15">
+                <li><a href="#"><i class="fa fa-facebook text-white"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter text-white"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus text-white"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram text-white"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin text-white"></i></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-8">
+            <div class="widget no-border m-0">
+              <ul class="list-inline pull-right sm-pull-none sm-text-center mt-5">
+                <li>
+                  <a href="#" class="text-white">FAQ</a>
+                </li>
+                <li class="text-white">|</li>
+                <li>
+                  <a href="#" class="text-white">Help Desk</a>
+                </li>
+                <li class="text-white">|</li>
+                <li>
+                  <a href="#" class="text-white">Support</a>
+                </li>
+                <li class="m-0 pl-10 pr-10"> <i class="fa fa-envelope-o text-white"></i> <a href="#" class="text-white">contact@yourdomain.com</a>
+                </li>
+              </ul>
+            </div><div class="clearfix"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="header-nav">
+      <div class="header-nav-wrapper navbar-scrolltofixed bg-light">
+        <div class="container">
+          <nav id="menuzord" class="menuzord default bg-light">
+            <a class="menuzord-brand pull-left flip xs-pull-center mb-15" href="index-mp-layout1.html"><img src="images/logo-wide.png" alt=""></a>
+            <ul class="menuzord-menu onepage-nav ">
+              <li class="active"><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#causes">Causes</a></li>
+              <li><a href="#team">Volunteer</a></li>
+              <li><a href="#gallery">Gallery</a></li>
+              <li><a href="#blog">Blog</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+            <a class="btn btn-sm btn-theme-colored pull-right flip hidden-sm hidden-xs mt-20 ajaxload-popup" href="ajax-load/donation-form.html" >Donate Now</a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
+
+<style lang="scss" scoped>
+#header{
+  margin: 0;
+  padding: 0;
+  justify-content: space-between;
+}
+</style>
